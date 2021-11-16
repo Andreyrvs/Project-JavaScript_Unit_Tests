a6f81e9feef5dafaefb57d7f568e74d8256febdf
Andreyrvs <cortaesai@gmail.com>
2021-11-16 20:27:59 -0300

    Requisito 06

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -25,16 +25,28 @@ const productDetails = require('../src/productDetails');
   ]
 
 */
-
+// console.log(productDetails('Alcool gel', 'batata'));
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function')
     // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
+    // expect(typeof productDetails('Alcool gel', 'Máscara')).toBe('array')
+    const tamanho = productDetails('Alcool gel', 'Máscara').length
+    expect(tamanho).toBe(2)
+    expect(productDetails('Alcool gel', 'Máscara')).toEqual([expect.any(Object), expect.any(Object)])
+    
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+
     // Teste se os dois productIds terminam com 123.
+    const terminam123 = productDetails('Alcool gel', 'Máscara')[1].details.productId
+    console.log(terminam123);
+    expect(terminam123).toHaveBeenLastCalledWith('123')
   });
 });
+/* 
+fail('Teste vazio!');
+ESCREVA SEUS TESTES ABAIXO:
+Teste se productDetails é uma função.
+Teste se o array retornado pela função contém dois itens dentro.
+Teste se os dois itens dentro do array retornado pela função são objetos.
+ */
